d2116e7223ec9b0d09242ffd3b1f9e8042eacbb4
Puyuan Deng <paulliwali@hotmail.com>
2013-08-20 16:33:24 -0400

    Updated setup.sh to include new .vimrc dotfile

--- setup.sh
+++ setup.sh
@@ -39,10 +39,12 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone hhttps://github.com/paulliwali/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
+ln -sb dotfiles/.vimrc .
+
 
